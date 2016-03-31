70b85ff7e1245fc44e290b2e34bcd8e639a7fcba
vitorhm <vitorhsm14@gmail.com>
2016-03-31 15:03:09 -0300

    31/03/2016 - 15:03

--- app/src/main/java/br/com/placesnap/controller/activity/NewPostActivity.java
+++ app/src/main/java/br/com/placesnap/controller/activity/NewPostActivity.java
@@ -3,6 +3,7 @@ package br.com.placesnap.controller.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -27,5 +28,14 @@ public class NewPostActivity extends AppCompatActivity {
         mTextViewInstruction = (TextView) findViewById(R.id.textview_new_post_instruction);
         mEditTextDescription = (EditText) findViewById(R.id.edittext_new_post_description);
         mImageViewPost = (ImageView) findViewById(R.id.imageview_new_post_image);
+
+        mTextViewInstruction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/br/com/placesnap/controller/activity/PhotoActivity.java
@@ -0,0 +1,47 @@
+package br.com.placesnap.controller.activity;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import br.com.placesnap.R;
+import br.com.placesnap.controller.view.CameraPreview;
+
+public class PhotoActivity extends AppCompatActivity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+    }
+
+    private boolean checkCameraHardware(Context context) {
+        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+
+    @SuppressWarnings("deprecation")
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+}
