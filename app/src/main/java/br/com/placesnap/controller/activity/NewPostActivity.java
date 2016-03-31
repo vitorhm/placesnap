6ec8739d5e77f6746b4ea767dc49bccfd9682c7b
vitorhm <vitorhsm14@gmail.com>
2016-03-31 10:13:03 -0300

    aSO KASIOJ SDOIF SI0V SOIVKO SKVS

--- app/src/main/java/br/com/placesnap/controller/activity/NewPostActivity.java
+++ app/src/main/java/br/com/placesnap/controller/activity/NewPostActivity.java
@@ -2,16 +2,30 @@ package br.com.placesnap.controller.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import br.com.placesnap.R;
 
 public class NewPostActivity extends AppCompatActivity {
-    private TextView textViewDescription;
+    private TextView mTextViewInstruction;
+    private EditText mEditTextDescription;
+    private ImageView mImageViewPost;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
+
+        this.bindViews();
+    }
+
+    private void bindViews() {
+        mTextViewInstruction = (TextView) findViewById(R.id.textview_new_post_instruction);
+        mEditTextDescription = (EditText) findViewById(R.id.edittext_new_post_description);
+        mImageViewPost = (ImageView) findViewById(R.id.imageview_new_post_image);
     }
 }
